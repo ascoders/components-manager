1e8c65f4dae169e23f1e4007a90e68918c44267d
ascoders <576625322@qq.com>
2017-04-27 16:09:53 +0800

    update

--- src/clean.ts
+++ src/clean.ts
@@ -5,7 +5,7 @@ import * as colors from 'colors'
 import * as fse from 'fs-extra'
 
 /**
- * 将每个组件的产出目录清空
+ * 将每个组件的产出目录删除
  */
 export default (managerConfig: ManagerConfig) => {
   managerConfig.components.forEach(config => {
@@ -14,7 +14,8 @@ export default (managerConfig: ManagerConfig) => {
     }
 
     const outputPath = path.join(process.cwd(), config.root, config.outputDir)
-    fse.ensureDirSync(outputPath)
-    fse.emptyDirSync(outputPath)
+    if (fs.existsSync(outputPath)) {
+      fse.removeSync(outputPath)
+    }
   })
 }
